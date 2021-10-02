70769a57a5e9d5e094726c3f74b540d06d32ce36
DarkHornet15 <91774471+DarkHornet15@users.noreply.github.com>
2021-10-02 12:10:26 +0530

    Create hash.java
    Create hashing for finding duplicate of files

---
+++ hash.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class hash {
+    static void createHashMap(int arr[]) {
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            Integer c = hmap.get(arr[i]);
+            if (hmap.get(arr[i]) == null) {
+                hmap.put(arr[i], 1);
+            } else {
+                hmap.put(arr[i], ++c);
+            }
+        }
+        System.out.println(hmap);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 11, 35, 6, 11, 4, 6, 12 };
+        createHashMap(arr);
+    }
+}
